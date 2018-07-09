668bb54189b9d2c572bfdd7543978c4baa0d7564
Moo <moo_jhingjit@hotmail.com>
2018-07-10 00:17:49 +0700

    Do receipt

--- src/Libraries/pdf.js
+++ src/Libraries/pdf.js
@@ -1,10 +1,10 @@
 import JsPDF from 'jspdf'
-import font from './fonts/THSarabun.ttf'
+// import font from './fonts/THSarabun.ttf'
 const CREATE_PDF = (html) => {
   let doc = new JsPDF()
-  doc.addFileToVFS(font)
-  doc.addFont(font, 'THsarabun', 'normal')
-  doc.setFont('THsarabun')
+  // doc.addFileToVFS(font)
+  // doc.addFont(font, 'THsarabun', 'normal')
+  // doc.setFont('THsarabun')
   doc.fromHTML(html, 15, 15)
   doc.save('file.pdf')
 }
--- src/Pages/Ordering/receipt-template.vue
+++ src/Pages/Ordering/receipt-template.vue
@@ -1,6 +1,167 @@
 <template>
   <div ref="receipt" class="receipt">
-    {{local.storeName}}
+    <table border="1" class="main-table">
+      <tr>
+        <td>main-table</td>
+      </tr>
+    </table>
+    <!-- <table class="main-table" border="1" width="100%">
+      <thead>
+        <tr style="height: 94px;">
+          <td style="height: 94px;">
+            <table width="100%" class="header-table">
+              <tbody>
+                <tr>
+                  <td style="width:50%;">logo</td>
+                  <td class="text-right" style="width:50%;">Orders</td>
+                </tr>
+                <tr>
+                  <td style="width:50%;">xxxxxxxxxxxxxx</td>
+                  <td class="text-right" style="width:50%;"></td>
+                </tr>
+                <tr>
+                  <td style="width:50%;">xxxxxxxxxxxxxx</td>
+                  <td class="text-right" style="width:50%;"></td>
+                </tr>
+                <tr>
+                  <td style="width:50%;">xxxxxxxxxxxxxx</td>
+                  <td class="text-right" style="width:50%;"></td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </thead>
+    <tbody class="border">
+      <tr>
+        <td class="no-padding border">
+          <table width="100%" class="receiver">
+            <tbody>
+              <tr>
+                <td style="width: 50%;">xxxxxx</td>
+                <td class="text-right" style="width: 50%;">xxxxxx</td>
+              </tr>
+              <tr>
+                <td style="width: 50%;">xxxxxx</td>
+                <td class="text-right" style="width: 50%;">xxxxxx</td>
+              </tr>
+              <tr>
+                <td style="width: 50%;">xxxxxx</td>
+                <td class="text-right" style="width: 50%;">xxxxxx</td>
+              </tr>
+              <tr>
+                <td style="width: 50%;">xxxxxx</td>
+                <td class="text-right" style="width: 50%;"></td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td class="no-padding">
+          <table width="100%" class="item-lists">
+            <thead>
+              <tr>
+                <td width="5%">xxxxxx</td>
+                <td width="60%">xxxxxx</td>
+                <td width="5%">xxxxxx</td>
+                <td width="5%">xxxxxx</td>
+                <td width="10%">xxxxxx</td>
+                <td width="5%">xxxxxx</td>
+                <td width="10%">xxxxxx</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+                <td class="text-right">xx</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colspan="2" rowspan="3">xxxxxx</td>
+                <td colspan="3">xxxxxx</td>
+                <td colspan="2" class="text-right">-</td>
+              </tr>
+              <tr>
+                <td colspan="3">xxxxxx</td>
+                <td colspan="2" class="text-right">-</td>
+              </tr>
+              <tr>
+                <td colspan="3">xxxxxx</td>
+                <td colspan="2" class="text-right">-</td>
+              </tr>
+            </tfoot>
+          </table>
+        </td>
+      </tr>
+      <tr>
+        <td class="no-padding border">
+          <table width="100%" class="condition">
+            <tbody>
+              <tr>
+                <td class="text-center" style="width: 50%;">xxxxxx</td>
+                <td class="text-center" style="width: 50%;">xxxxxx</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="width: 50%;">xxxxxx xxxxxx xxxxxx</td>
+                <td class="text-center" style="width: 50%;">xxxxxxxxxxxxxxxxxxxxxxxx</td>
+              </tr>
+              <tr>
+                <td class="text-center" style="width: 50%;">xxxxxxxxxxxxxxxxxxxxxxxx</td>
+                <td class="text-center" style="width: 50%;">xxxxxx</td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+      <tfoot>
+        <tr>
+          <td class="no-padding">
+            <table width="100%" class="signature">
+              <tbody>
+                <tr>
+                  <td class="text-center" style="width: 50%;">xxxxxx..........................................  </td>
+                  <td class="text-center" style="width: 50%;">xxxxxx.....................................</td>
+                </tr>
+                <tr>
+                  <td class="text-center" style="width: 50%;">xxxxxx  xxxxxx</td>
+                  <td class="text-center" style="width: 50%;">(xxxxxx    xxxxxx)</td>
+                </tr>
+                <tr>
+                  <td class="text-center" style="width: 50%;">087-5070161 (xxxxxx)</td>
+                  <td class="text-center" style="width: 50%;"></td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tfoot>
+    </table> -->
   </div>
 </template>
 
@@ -40,7 +201,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  p{
-    color: green;
+p{
+  color: #ff0000;
+}
+  .main-table{
+    border: 1px solid #ccc;
+    border-collapse: collapse;
+    font-size: 13px;
+  }
+  .main-table tr td table tr td{
+    padding: 5px;
+  }
+  .main-table .item-lists{
+    border-collapse: collapse;
+  }
+  .main-table .item-lists tr td{
+    border: 1px solid #ccc;
+    border-collapse: collapse;
+  }
+  .item-lists thead tr td {
+    text-align: center;
+    font-weight: bold;
+  }
+  .item-lists tfoot tr td {
+    text-align: center;
+  }
+  .signature{
+    margin: 20px 0;
+  }
+  .border{
+    border: 1px solid;
+  }
+  .no-padding{
+    padding: 0 !important;
+  }
+  .text-right{
+    text-align: right !important;
+  }
+  .text-center{
+    text-align: center;
   }
 </style>
