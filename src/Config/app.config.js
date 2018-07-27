6a426c6cb05f0cba494d68fb683072a6b00c65f4
Moo <moo_jhingjit@hotmail.com>
2018-07-27 14:24:26 +0700

    Fix uri api

--- src/Config/app.config.js
+++ src/Config/app.config.js
@@ -4,8 +4,8 @@
 //   url = `${window.location.origin}/dashboard-api`
 // }
 const api = {
-  'apiEndPoint': 'http://localhost:3000/api',
-  // 'apiEndPoint': `${window.location.protocol}//${window.location.hostname}:3000/api`, // for production
+  // 'apiEndPoint': 'http://localhost:3000/api',
+  'apiEndPoint': `${window.location.protocol}//${window.location.hostname}:3000/api`, // for production
   'app': {
     'resource': '/app/resource'
   },
